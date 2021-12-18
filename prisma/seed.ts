bae664f902012993557f61a5779720a3f5b08a16
Pratik Panchal <pnchlprtk@gmail.com>
2021-12-18 12:43:03 +0000

    Seed setup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,24 @@
-import { restaurants } from "./restaurants";
-import { PrismaClient } from "@prisma/client";
+import Prisma, * as PrismaAll from "@prisma/client";
+import * as fs from "fs";
 
+const PrismaClient = Prisma?.PrismaClient || PrismaAll?.PrismaClient;
 const prisma = new PrismaClient();
 
+let restaurants = JSON.parse(
+  fs.readFileSync("./prisma/restaurants.json").toString()
+);
+
 async function main() {
-    await prisma.restaurant.createMany({
-        data: restaurants
-    });
+  await prisma.restaurant.createMany({
+    data: restaurants,
+  });
 }
 
-main().catch(e => {
+main()
+  .catch((e) => {
     console.log(e);
     process.exit(1);
-}).finally(() => {
+  })
+  .finally(() => {
     prisma.$disconnect();
-});
+  });

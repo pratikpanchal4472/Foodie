431b562b75982fbbdf53ade3fdf35e08eafa8185
Pratik Panchal <pnchlprtk@gmail.com>
2021-12-28 15:10:31 +0000

    Schema update to have menu items

---
+++ src/routes/menuitems/_api.ts
@@ -0,0 +1,72 @@
+import type { Request } from "@sveltejs/kit";
+import PrismaClient from "$lib/prisma";
+
+const prisma = new PrismaClient();
+
+export const api = async (request: Request, data?: Record<string, unknown>) => {
+  let body = {};
+  let status = 500;
+
+  switch (request.method.toUpperCase()) {
+    case "GET":
+      body = await prisma.menuItem.findMany({
+        where: {
+          restaurantId: Number(request.query.get('rId'))
+        }
+      });
+      status = 200;
+      break;
+    case "POST":
+      body = await prisma.menuItem.create({
+        data: {
+          name: data.name as string,
+          category: data.category as string,
+          unitPrice: data.rating as number,
+          restaurantId: data.restaurantId as number
+        },
+      });
+      status = 201;
+      break;
+    case "DELETE":
+      body = await prisma.menuItem.delete({
+        where: {
+          id: Number(request.params.id),
+        },
+      });
+      status = 200;
+      break;
+    case "PATCH":
+      body = await prisma.menuItem.update({
+        where: {
+          id: Number(request.params.id),
+        },
+        data: {
+          unitPrice: data.unitPrice,
+          name: data.name,
+          category: data.category,
+        },
+      });
+      status = 200;
+      break;
+
+    default:
+      break;
+  }
+
+  if (
+    request.method.toUpperCase() !== "GET" &&
+    request.headers.accept !== "application/json"
+  ) {
+    return {
+      status: 303,
+      headers: {
+        location: "/",
+      },
+    };
+  }
+
+  return {
+    status,
+    body,
+  };
+};
--- src/routes/restaurant/[id].svelte
+++ src/routes/restaurant/[id].svelte
@@ -1,12 +1,12 @@
 <script context="module" lang="ts">
   import type { Load } from "@sveltejs/kit";
 
+  let menuItems = [];
+
   export const load: Load = async ({ page }) => {
     const id = page.params.id;
-
+menuItems = 
   };
 </script>
 
-<div class="col">
-    
-</div>
+<div class="col" />
